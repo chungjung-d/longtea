fb2db8225e681eb09048b2bc9abddf326a7e2257
exit <chungjung.d@gmail.com>
2023-07-15 02:33:02 +0900

    longtea script hotfix

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 sudo apt update && sudo apt upgrade -y
 sudo apt install -y git curl 
 
-curl -o longtea https://github.com/chungjung-d/longtea/releases/download/v0.1.0/longtea_v0.1.0
-curl -o longteac https://github.com/chungjung-d/longteac/releases/download/v0.1.0/longteac_v0.1.0
+sudo wget https://github.com/chungjung-d/longtea/releases/download/v0.1.0/longtea_v0.1.0 -O longtea
+sudo wget https://github.com/chungjung-d/longteac/releases/download/v0.1.0/longteac_v0.1.0 -O longteac
 
-chmod +x longtea longteac
+sudo chmod +x longtea longteac
 sudo mv longtea longteac /usr/local/bin
 

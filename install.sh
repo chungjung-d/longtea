3a62e23da112ea248ae421b25e8127508caeea5e
chungjung-d <63407866+chungjung-d@users.noreply.github.com>
2023-07-15 16:36:15 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y git curl 
+sudo apt install -y git wget 
 
 sudo wget https://github.com/chungjung-d/longtea/releases/download/v0.1.0/longtea_v0.1.0 -O longtea
 sudo wget https://github.com/chungjung-d/longteac/releases/download/v0.1.0/longteac_v0.1.0 -O longteac
